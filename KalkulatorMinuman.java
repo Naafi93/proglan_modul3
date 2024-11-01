a6f389f7411283dcd875e39307301f218a1b74f7
Naafi93 <armazzcb@gmail.com>
2024-11-01 13:54:51 +0700

    hehehe

--- KalkulatorMinuman.java
+++ KalkulatorMinuman.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class KalkulatorMinuman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Masukkan kalori minuman: ");
+        out.print("Masukkan kalori minuman: ");
         double kaloriMinuman = scanner.nextDouble();
 
-        System.out.print("Masukkan gula (gram): ");
+        out.print("Masukkan gula (gram): ");
         double gula = scanner.nextDouble();
 
         // Asumsikan 1 gram gula = 4 kalori
@@ -16,7 +18,7 @@ public class KalkulatorMinuman {
         // Menghitung total kalori
         double totalKalori = kaloriMinuman + kaloriDariGula;
 
-                System.out.println("Total kalori minuman: " + totalKalori);
+        out.println("SUDAH SAYA GANTI YA KACK: " + totalKalori);
 
         scanner.close(); // Menutup scanner untuk mencegah kebocoran sumber daya
     }
